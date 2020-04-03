7fffcc56f06362172ed8fb5cc80d1229dcf5dada
Asta86 <psljp@protonmail.com>
2020-04-03 20:23:51 -0300

    Update CORS config

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const ctrl     = require('./src/analyticsController');
-      cors     = require('cors'),
       dotenv   = require('dotenv'),
       express  = require('express'),
       mongoose = require('mongoose'),
@@ -11,21 +10,24 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI);
 
-const corsOptions = {
-  origin: function(origin, callback) {
-    const allowedOrigins = process.env.ALLOWED_HOSTS.split(' '),
-          allowNoOrigin = (!origin || origin === 'null') && Boolean(process.env.ALLOW_NO_ORIGIN),
-          allowAccess = allowNoOrigin || allowedOrigins.includes(origin);
-    if (allowAccess) {
-      callback(null, true);
-    } else {
-      let msg = 'Access denied by CORS policy.';
-      callback(new Error(msg), false);
-    }
-  }
-};
+app.use(function (req, res, next) {
 
-app.use(cors(corsOptions));
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://asta86.gitlab.io');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use(express.urlencoded({ extended: false }));
